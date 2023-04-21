f20bed97da1491a6433f6bd1c703350dbff2426c
Kataruse <113931857+Kataruse@users.noreply.github.com>
2023-04-21 17:41:33 -0400

    Update Score.cs
    Forgot to add the header comment.

--- Score.cs
+++ Score.cs
@@ -1,4 +1,15 @@
-﻿using System;
+/**
+*--------------------------------------------------------------------
+* File name: Score.cs
+* Project name: 1260-002-CrumpNick-CollinsSam-FlahertyLogan-Project5
+*--------------------------------------------------------------------
+* Author’s name and email: Nick Crump, CRUMPNA@ETSU.EDU
+* Course-Section: CSCI 1260-002
+* Creation Date: 4/20/2023
+* -------------------------------------------------------------------
+*/﻿
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
